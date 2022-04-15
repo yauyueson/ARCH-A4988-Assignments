6e14b13bbc8a2db8a0c5627089dd39401325f338
yauyueson <98435949+yauyueson@users.noreply.github.com>
2022-04-15 17:40:29 -0400

    update

--- Exercise 12/script.js
+++ Exercise 12/script.js
@@ -31,12 +31,15 @@ const flowers = [
   
 
   //RENDER FLOWER TO WEBPAGE
+
+
   const ul = document.querySelector('ul')
 
 
   function renderFlowersToPage(flowers){
     for(i=0; i<flowers.length; i++){
         let list_item= document.createElement('li');
+        list_item.classList.add(flowers[i].color,'card')
 
         //flower name
         let title = document.createElement("h3");
@@ -47,11 +50,43 @@ const flowers = [
         //flower image
         let image = document.createElement("img");
         image.setAttribute("src",flowers[i].image);
-        ul.appendChild(list_item);
         list_item.appendChild(title);
         list_item.appendChild(color);
         list_item.appendChild(image);
+        ul.appendChild(list_item);
     }
   }
 
   renderFlowersToPage(flowers);
+
+  // FILTER FLOWERS BY COLOR
+
+  const filterBtns = document.querySelector('.filters')
+
+  const cards = document.querySelectorAll('.cards');
+
+
+  function filterFn(event){
+      
+    if(event.target.classList.contains("filter-btn")){
+
+      const filterValue = event.target.getAttribute('data-filter')
+
+      console.log("clicked",filterValue)
+
+      for(let i=0; i<cards.length; i++){
+        console.log(cards[i])
+
+        if(cards[i].classList.contains(filterValue) || filterValue === "all"){
+        cards[i].classList.remove("hide");
+        cards[i].classList.add("show");
+        }else{
+        cards[i].classList.add("hide");
+        cards[i].classList.remove("show");
+        }
+    }
+   }
+  }
+
+  
+  filterBtns.addEventListener('click', filterFn)
