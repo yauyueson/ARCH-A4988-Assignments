90649694e745595620d37f582c511d72a97a28b5
yauyueson <98435949+yauyueson@users.noreply.github.com>
2022-05-05 16:26:48 -0400

    May 5th Final Update

--- projects/p3-catalog/assets/script.js
+++ projects/p3-catalog/assets/script.js
@@ -8,13 +8,17 @@ const nft =[
      "Name": "CryptoPunk #3100",
      "Collection": "CryptoPunks",
      "Last Price": "ETH 4200",
+     "Price in Number": 4200,
+     "Price in USD": 11581584,
      "URL": "https:\/\/lh3.googleusercontent.com\/PWDq8erM2dMscd99OntjFRJFfvtvki7uxeYiBUT8e59Kdbn8s34dM59kCkVZ66b687B6i8KXMDspRfnU-JbLcB9Kc23EoSydJNkmgA=w600"
     },
     {
      "Serial No": 2,
      "Name": "CryptoPunk #9998",
      "Collection": "CryptoPunks",
-     "Last Price": "ETH 124457.0675",
+     "Last Price": "ETH 124457",
+     "Price in Number": 124457,
+     "Price in USD": 343192666.64,
      "URL": "https:\/\/lh3.googleusercontent.com\/O0TPreCr-fnuhYTUGwHPfp3gZgqwAogRrdmkm60Aiozg9kTuyMeIKc_A0I_yBNIJfoISRuGllSHsatOjxxMWHMMxMOhMbpOJ43wM8A=w600"
     },
     {
@@ -22,6 +26,8 @@ const nft =[
      "Name": "CryptoPunk #7804",
      "Collection": "CryptoPunks",
      "Last Price": "ETH 4200",
+     "Price in Number": 4200,
+     "Price in USD": 11581584,
      "URL": "https:\/\/lh3.googleusercontent.com\/mR1utxNugWurPayNNDqc5Dmk4gO7WTpdmlMN-9N3rdrC3hIl-8Z2lEpBtX6D9mPry8uDDrBNYDjNaFHHdFE9HVWL6_htFwL7-KXj=w600"
     },
     {
@@ -29,6 +35,8 @@ const nft =[
      "Name": "CryptoPunk #6965",
      "Collection": "CryptoPunks",
      "Last Price": "ETH 800",
+     "Price in Number": 800,
+     "Price in USD": 2206016,
      "URL": "https:\/\/lh3.googleusercontent.com\/ClS4KdMPO8_25m1yYx-oUJrgeFi_C85dJbfzOPFUJbR_SRCsyoHd4ZapoMvybh4jLWk3BFiMxUSn8CEA_EHrTsXy=w600"
     },
     {
@@ -36,6 +44,8 @@ const nft =[
      "Name": "CryptoPunk #1482",
      "Collection": "CryptoPunks",
      "Last Price": "ETH 74.5",
+     "Price in Number": 74.5,
+     "Price in USD": 205435.24,
      "URL": "https:\/\/lh3.googleusercontent.com\/DVO5lBicI3vBA24X4kM7hBe6ElMsa6waj95fa0ENasYnjfWrIvOx5az3d0YH2Wc_j8sLPB9LCwDA3tnpmItYlOORLY2h5COElQYv9Q=w600"
     },
     {
@@ -43,34 +53,44 @@ const nft =[
      "Name": "CryptoPunk #5217",
      "Collection": "CryptoPunks",
      "Last Price": "ETH 2250",
+     "Price in Number": 2250,
+     "Price in USD": 6204420,
      "URL": "https:\/\/lh3.googleusercontent.com\/x-8cxWfSABaoHdqyUvosBDeLr4c_6GO4YU17eHAKcF9-SwOj3FEMqQA_7mOwtg9glv7e6P8WnZuDR2_bPTiUvrai=w600"
     },
     {
      "Serial No": 7,
      "Name": "CryptoPunk #4036",
      "Collection": "CryptoPunks",
-     "Last Price": "ETH63",
+     "Last Price": "ETH 63",
+     "Price in Number": 63,
+     "Price in USD": 173723.76,
      "URL": "https:\/\/lh3.googleusercontent.com\/qqNZJwr0AEy5CuImQktvHem_4LBK_ST9IcstUeyj3D1o-fnMEjvdaT5fmpNDtnSkXvYZWA4QxxedRz1yB3CCcFg=w600"
     },
     {
      "Serial No": 8,
      "Name": "CryptoPunk #1321",
      "Collection": "CryptoPunks",
-     "Last Price": "ETH15.5",
+     "Last Price": "ETH 15.5",
+     "Price in Number": 15.5,
+     "Price in USD": 42741.56,
      "URL": "https:\/\/lh3.googleusercontent.com\/hBEz_urvzf15Y6AlCJPe0YCkt_XVNx5qBrnUsGMwPos57dvgQUC-0TEPdCVmiQ_OMpn3SlkWJacE2ZM35u_xBv2Q7dq5zj1crlP-Rw=w600"
     },
     {
      "Serial No": 9,
      "Name": "CryptoPunk #8334",
      "Collection": "CryptoPunks",
-     "Last Price": "ETH91.23",
+     "Last Price": "ETH 91.2",
+     "Price in Number": 91.23,
+     "Price in USD": 251568.5496,
      "URL": "https:\/\/lh3.googleusercontent.com\/HjTz1VbefDyk0xTAOCmGVQVx66MVRorfj5MKnV5KmMuW--QMkBfjNA1ob7YmC4Pp3p-dDkTkN5n19SyEmMCdwiBl5Zo6Kn4cl6oFbIU=w600"
     },
     {
      "Serial No": 10,
      "Name": "CryptoPunk #7241",
      "Collection": "CryptoPunks",
-     "Last Price": "ETH155",
+     "Last Price": "ETH 155",
+     "Price in Number": 155,
+     "Price in USD": 427415.6,
      "URL": "https:\/\/lh3.googleusercontent.com\/2qpG_GbdDyoMP0qyZMYKbf9XAxWqYkCAtQeCSm288RrLbtxNw0NiEiGX3tA-LqpIwErMChcYe-C-tPpPUMD6wD8-lYJ1Jtw04ymXpw=w600"
     },
     {
@@ -78,6 +98,8 @@ const nft =[
      "Name": "Bored Ape Yacht Club #3368",
      "Collection": "Bored Ape Yacht Club",
      "Last Price": "ETH 29.5",
+     "Price in Number": 29.5,
+     "Price in USD": 81346.84,
      "URL": "https:\/\/lh3.googleusercontent.com\/u-2FnHbaJ3U_KCDlmg2McX9Yfo7brsAzOffqihNXCGkHljA89SPPzwdjQiVSWcsvxCoj_ydBcDNCuZvHEekaYekaMEH4XX32k9US=w600"
     },
     {
@@ -85,6 +107,8 @@ const nft =[
      "Name": "Bored Ape Yacht Club #3650",
      "Collection": "Bored Ape Yacht Club",
      "Last Price": "ETH 69",
+     "Price in Number": 69,
+     "Price in USD": 190268.88,
      "URL": "https:\/\/lh3.googleusercontent.com\/1up7IvZnXj99OAavLkjdahY4ROjwqOKDvgmPxceirndDuW_YJUOToHGJldUI0LjtFTUFbHfikAMN5nr_uWxi1tcVtjhJN45DAwV_QQ=w600"
     },
     {
@@ -92,6 +116,8 @@ const nft =[
      "Name": "Bored Ape Yacht Club #9996",
      "Collection": "Bored Ape Yacht Club",
      "Last Price": "ETH 0.39",
+     "Price in Number": 0.39,
+     "Price in USD": 1075.4328,
      "URL": "https:\/\/lh3.googleusercontent.com\/oOxrPSqIusE3pR1vLRSTQwfn52LnwnsZ_a1nCXked6fwOHrOnhvMBM1uMpshUb8Q4lL-gnlV9YoteT9cXnDgIsz46Zkbo7CImK-yKw=w600"
     },
     {
@@ -99,6 +125,8 @@ const nft =[
      "Name": "Bored Ape Yacht Club #122",
      "Collection": "Bored Ape Yacht Club",
      "Last Price": "ETH 23",
+     "Price in Number": 23,
+     "Price in USD": 63422.96,
      "URL": "https:\/\/lh3.googleusercontent.com\/BLM8j8CkydHV2XuYC-ZB_bFiCFFjAbMPYCKr7BUVEPZpGnjfOdTcIwtJR-VKldUodiKn1qR-bypOLla0Ci9I_byu9OC70zjJHjZFWw=w600"
     },
     {
@@ -106,6 +134,8 @@ const nft =[
      "Name": "Bored Ape Yacht Club #8470",
      "Collection": "Bored Ape Yacht Club",
      "Last Price": "ETH 51",
+     "Price in Number": 51,
+     "Price in USD": 140633.52,
      "URL": "https:\/\/lh3.googleusercontent.com\/eqXJ2N23ycVCyKLKegfprn7sAdfeLUe4OpZ3i8i36lc07u-0jgiE6U2hM5bkHwRAvEyGhRktz2T1MD4Kbh4ojZn2f2GOKi-TB8Un2Q=w600"
     },
     {
@@ -113,6 +143,8 @@ const nft =[
      "Name": "Bored Ape Yacht Club #955",
      "Collection": "Bored Ape Yacht Club",
      "Last Price": "ETH 140",
+     "Price in Number": 140,
+     "Price in USD": 386052.8,
      "URL": "https:\/\/lh3.googleusercontent.com\/7Wg2BOR6Y19roz1O4ZbHucCQQvYiseV47tOYgy8Jp27vlLeUPGXPwrYH1aAQZJ8n88XL2F9McN56RzM8EBQbd-RUsNmHIbx6Swu60Jw=w600"
     },
     {
@@ -120,6 +152,8 @@ const nft =[
      "Name": "Bored Ape Yacht Club #4303",
      "Collection": "Bored Ape Yacht Club",
      "Last Price": "ETH 95",
+     "Price in Number": 95,
+     "Price in USD": 261964.4,
      "URL": "https:\/\/lh3.googleusercontent.com\/a0Hq33FbaDYQB5SqSlwyb2AUYOcZYaa2JoadJVoegfG26546E7ARj6BAlG3NX5yOGtNNTw6JcTwiRO8qEmcpNBcSyw8bg9W5eqw90Q=w600"
     },
     {
@@ -127,6 +161,8 @@ const nft =[
      "Name": "Bored Ape Yacht Club #9419",
      "Collection": "Bored Ape Yacht Club",
      "Last Price": "ETH 73.5",
+     "Price in Number": 73.5,
+     "Price in USD": 202677.72,
      "URL": "https:\/\/lh3.googleusercontent.com\/j_0RWoaLct0GOHnW4EGpT9p-u1yIpSkd6R8JWKbg_9AoiP9HMKTZj2GKETacOgIAlL--7irqVFwqTGqDrrgaxJMoaw2-NrR-IvE0=w600"
     },
     {
@@ -134,6 +170,8 @@ const nft =[
      "Name": "Bored Ape Yacht Club #1455",
      "Collection": "Bored Ape Yacht Club",
      "Last Price": "ETH 47.99",
+     "Price in Number": 47.99,
+     "Price in USD": 132333.3848,
      "URL": "https:\/\/lh3.googleusercontent.com\/NTqLmufORLnsDfM7PXMy8CKAU9Hi4F1SoSqAVjDntISqNc0TPEi0oEM08MCYgOureqWAADfZt-h3x8NDzrD6Heme1gCqge4KYxrAWw=w600"
     },
     {
@@ -141,13 +179,17 @@ const nft =[
      "Name": "Bored Ape Yacht Club #2180",
      "Collection": "Bored Ape Yacht Club",
      "Last Price": "ETH 97",
+     "Price in Number": 97,
+     "Price in USD": 267479.44,
      "URL": "https:\/\/lh3.googleusercontent.com\/M8AUbgTlaOl6gMTDHh0maDiUXiyKFGlOPjspVoPDTs_Cd5Xujttwk7lvi4FFkCJXREND3edb99cERHc4wKpP4tiGMMekH-_Eva_R9A=w600"
     },
     {
      "Serial No": 21,
      "Name": "Mutant Ape Yacht Club #15352",
      "Collection": "Mutant Ape Yacht Club",
-     "Last Price": "ETH 69",
+     "Last Price": "WETH 69",
+     "Price in Number": 69,
+     "Price in USD": 190268.88,
      "URL": "https:\/\/lh3.googleusercontent.com\/-Yl4DMKOJhcNSRVqJCQ57itbUKN97YzRMiIgCk8aU7drYiFQYYq8cl5FlHUZLbxAhCyJiQ3P7zzijbfyhZHghFpNRGQW6SNqiS_i=w600"
     },
     {
@@ -155,20 +197,26 @@ const nft =[
      "Name": "Mutant Ape Yacht Club #8810",
      "Collection": "Mutant Ape Yacht Club",
      "Last Price": "WETH 24.5",
+     "Price in Number": 24.5,
+     "Price in USD": 67559.24,
      "URL": "https:\/\/lh3.googleusercontent.com\/8ir3j_xTVQ4-4wAZ5U0uOiLc7Jc6PJuGhIvSdLKr5r3zVBWJiNhrZu2z7MFTHVg5IlmEu5LR2o9Hpl_lVzu0lGWh5gPZ9cJL8Ifq9Q=w600"
     },
     {
      "Serial No": 23,
      "Name": "Mutant Ape Yacht Club #6044",
      "Collection": "Mutant Ape Yacht Club",
-     "Last Price": "WETH 25.4985",
+     "Last Price": "WETH 25.49",
+     "Price in Number": 25.49,
+     "Price in USD": 70289.18479999999,
      "URL": "https:\/\/lh3.googleusercontent.com\/FRnfoqP_aB1H-ZG5mw13pBPeH9765AnXXxsFNZg6vnpux_46WqDTswvVjXyKhwTSWGIlfEp5gG_8RUdCOIbTpteK0clxJCcgBf6e-g=w600"
     },
     {
      "Serial No": 24,
      "Name": "Mutant Ape Yacht Club #13856",
      "Collection": "Mutant Ape Yacht Club",
-     "Last Price": "WETH 0.2496",
+     "Last Price": "WETH 0.24",
+     "Price in Number": 0.24,
+     "Price in USD": 661.8048,
      "URL": "https:\/\/lh3.googleusercontent.com\/llAeu8wuoTdtwJLDy7xe6LOw5-Dj_TPAKs5fU9X9M7fwgHDmXfz0xeehpu1YPEUj1AxT4oulLxRIQYtatzCWo3C5Zl3ifjUWVnMFuyM=w600"
     },
     {
@@ -176,6 +224,8 @@ const nft =[
      "Name": "Mutant Ape Yacht Club #17107",
      "Collection": "Mutant Ape Yacht Club",
      "Last Price": "WETH 26.01",
+     "Price in Number": 26.01,
+     "Price in USD": 71723.09520000001,
      "URL": "https:\/\/lh3.googleusercontent.com\/hZb9LN5zTOhUdEkaSgXPJPoSXH7rMQguG4_dxIsF0EvmwJ2C9dN7VU5l2KP66BlfdLgJRiPvH82zOfp0XUhOZJbpzVhCC_eC4QOi3w=w600"
     },
     {
@@ -183,6 +233,8 @@ const nft =[
      "Name": "Mutant Ape Yacht Club #7208",
      "Collection": "Mutant Ape Yacht Club",
      "Last Price": "WETH 26.28",
+     "Price in Number": 26.28,
+     "Price in USD": 72467.6256,
      "URL": "https:\/\/lh3.googleusercontent.com\/z58CQCG3qsaSyJINKelfLCFgMjtCJrVXUf330oWKW-pFBvHQFdXhN0eIDNLF0r0Ak4QU-W9xMXZSkDSiPKN-T8f6ZqnXeLyWhIad=w600"
     },
     {
@@ -190,6 +242,8 @@ const nft =[
      "Name": "Mutant Ape Yacht Club #861",
      "Collection": "Mutant Ape Yacht Club",
      "Last Price": "ETH 51",
+     "Price in Number": 51,
+     "Price in USD": 140633.52,
      "URL": "https:\/\/lh3.googleusercontent.com\/uWYUcbkrCp_l6DmKEx5wI8wLC209jdfHokZY-IRCrNAk8mQnCqSEexo32itUtqXtut5FXLO0PGyi8s8xBDAKeS4BUmyKqLoH188yDA=w600"
     },
     {
@@ -197,13 +251,17 @@ const nft =[
      "Name": "Mutant Ape Yacht Club #2196",
      "Collection": "Mutant Ape Yacht Club",
      "Last Price": "WETH 25.51",
+     "Price in Number": 25.51,
+     "Price in USD": 70344.3352,
      "URL": "https:\/\/lh3.googleusercontent.com\/p8m4ceuIDgOhk9m6TjXQuAA_yOON0t3UxCSYl4AutjEO2M0UoVix4rlH16_Pnj5bKRXjJK2cqgkAHwpxFOVRDp-h8fkPFLhxLcA0BA=w600"
     },
     {
      "Serial No": 29,
      "Name": "Mutant Ape Yacht Club #4849",
      "Collection": "Mutant Ape Yacht Club",
-     "Last Price": "WETH 22.928",
+     "Last Price": "WETH 22.92",
+     "Price in Number": 22.92,
+     "Price in USD": 63202.358400000005,
      "URL": "https:\/\/lh3.googleusercontent.com\/dG9YdzvAUCNMhB9cwDLziLFWoHgGytlCxAQ-uJ67USsch7-6iZu5kvgMcMB8WGpbXRXdq7A86Z-P9lUvceQ-MaSUTsAP2qYEKo_0dg=w600"
     },
     {
@@ -211,6 +269,8 @@ const nft =[
      "Name": "Mutant Ape Yacht Club #15290",
      "Collection": "Mutant Ape Yacht Club",
      "Last Price": "WETH 28",
+     "Price in Number": 28,
+     "Price in USD": 77210.56,
      "URL": "https:\/\/lh3.googleusercontent.com\/VjcjwhsEOK-JRYdhJ95mXoz6PB3yfnqYRZ8gi7O8_1NAUdUCaKlP-YvHS7LHrsPREHccEnWmtRJ1r2mTqymb8UfuKVTuv6iDc7u8NQ=w600"
     },
     {
@@ -218,6 +278,8 @@ const nft =[
      "Name": "Azuki #2892",
      "Collection": "Azuki",
      "Last Price": "WETH 2",
+     "Price in Number": 2,
+     "Price in USD": 5515.04,
      "URL": "https:\/\/lh3.googleusercontent.com\/6eCZ80yOwiDInlqFpl1co-KY2tWAqK9hjAFTTyrdVNLnW3uVXYSFOzeyqQCF6IOL1toaQ0wHaFKPZF7fr_BaVrF9bkrDs4gwna-p=w600"
     },
     {
@@ -225,6 +287,8 @@ const nft =[
      "Name": "Azuki #1102",
      "Collection": "Azuki",
      "Last Price": "WETH 2",
+     "Price in Number": 2,
+     "Price in USD": 5515.04,
      "URL": "https:\/\/lh3.googleusercontent.com\/nvDfmXW5LGyS4nCP94LDzM7dfF--AixcFVrwbHkgLl0OlIgk3LROoizW7VOnrLwZ5N773PEuQlr2m76GHN0XB1sK4QzWVRf3qNPQ9tE=w600"
     },
     {
@@ -232,6 +296,8 @@ const nft =[
      "Name": "Azuki #7103",
      "Collection": "Azuki",
      "Last Price": "WETH 2.9",
+     "Price in Number": 2.9,
+     "Price in USD": 7996.808,
      "URL": "https:\/\/lh3.googleusercontent.com\/IS4ySvhI8jGQsg_2fNFT3127JK61umYOGkulDrgVNxkBMeXz6acFjWjK1hmMBnMHPYnUq95NvmbBtUnfso8oR9GpRx_yL8L3w5lm=w600"
     },
     {
@@ -239,6 +305,8 @@ const nft =[
      "Name": "Azuki #5622",
      "Collection": "Azuki",
      "Last Price": "WETH 2.9",
+     "Price in Number": 2.9,
+     "Price in USD": 7996.808,
      "URL": "https:\/\/lh3.googleusercontent.com\/X4e1zA-nKpStOn-08GgZj7p3T_rj25Ek-lHaSVBb4_xCyC2xVFr8x7Ot_zMjhjHaIocfr69d3UIwFbNlm7EBRjAIZEyri4ppAobnhg=w600"
     },
     {
@@ -246,6 +314,8 @@ const nft =[
      "Name": "Azuki #3222",
      "Collection": "Azuki",
      "Last Price": "WETH 2.9",
+     "Price in Number": 2.9,
+     "Price in USD": 7996.808,
      "URL": "https:\/\/lh3.googleusercontent.com\/H-_u3B0UJNSZib96-nEbADPMv3wOU5JAd9MLY-xayWG_W5GcyOPhvefu9V8jHn2Y7DNeY4Y6qKIEm3-YR-oBRuJIZXWi8EeiAaFesA=w600"
     },
     {
@@ -253,55 +323,71 @@ const nft =[
      "Name": "Azuki #2362",
      "Collection": "Azuki",
      "Last Price": "WETH 10",
+     "Price in Number": 10,
+     "Price in USD": 27575.2,
      "URL": "https:\/\/lh3.googleusercontent.com\/TE48KfhWwfn0c_EYFsvSCpzWtemyv0hi4U7z7K2LiA-rnC859lSGfkewCqN22AACl2Yk3S4_6tMCOW2VvyuC-ozjxX3ZIug1hNBGuA=w600"
     },
     {
      "Serial No": 37,
      "Name": "Azuki #2903",
      "Collection": "Azuki",
-     "Last Price": "WETH 16.6183",
+     "Last Price": "WETH 16.61",
+     "Price in Number": 16.61,
+     "Price in USD": 45802.4072,
      "URL": "https:\/\/lh3.googleusercontent.com\/aHNYkgTz17mImz12xhxqEtnmRg-08JZd6pBmahSHqcGFNJv2qP2nS8R_75mrSBjrBcCKx0wTOYowo9_qD0dXDm_lvAbzwNralZa1zw=w600"
     },
     {
      "Serial No": 38,
      "Name": "Azuki #9107",
      "Collection": "Azuki",
-     "Last Price": "WETH 16.6183",
+     "Last Price": "WETH 16.61",
+     "Price in Number": 16.61,
+     "Price in USD": 45802.4072,
      "URL": "https:\/\/lh3.googleusercontent.com\/ACeKtE7bMnIVYey3DfdM6DWTOt216fOC16MLEPCVp3rAw19IUogxmZtA_DhPbDu9dAnQlr_m0IHMf1p64sGq1bll14Jk_WNdBJjY=w600"
     },
     {
      "Serial No": 39,
      "Name": "Azuki #9183",
      "Collection": "Azuki",
-     "Last Price": "WETH 16.6183",
+     "Last Price": "WETH 16.61",
+     "Price in Number": 16.61,
+     "Price in USD": 45802.4072,
      "URL": "https:\/\/lh3.googleusercontent.com\/usaYBdGv3CX1R2bjph0OKkELUtL_8hMMmT61RacNWWPK2uCgBGEQH-rHUk9zJ6V1lAM2Fbzv3KJucB7job8mcSeP4Z8mLJLjFXln=w600"
     },
     {
      "Serial No": 40,
      "Name": "Azuki #4035",
      "Collection": "Azuki",
-     "Last Price": "WETH 16.6183",
+     "Last Price": "WETH 16.61",
+     "Price in Number": 16.61,
+     "Price in USD": 45802.4072,
      "URL": "https:\/\/lh3.googleusercontent.com\/R1i0cndlxRVg9YNhDK8Z7zefYxy1SDbhEjxB_GNwYVjMq_CsPxSOZ1hKYZxcOz3rmCqdfwVJ5GcWK0AG2q7uyZ6hrp1aOBV66Gcaqw=w600"
     },
     {
      "Serial No": 41,
      "Name": "CloneX #16492",
      "Collection": "CLONE X - X TAKASHI MURAKAMI",
-     "Last Price": "WETH 0.069",
+     "Last Price": "WETH 0.06",
+     "Price in Number": 0.06,
+     "Price in USD": 165.4512,
      "URL": "https:\/\/lh3.googleusercontent.com\/eblhqfZIUgon6zfE8uAzBygABxdOb7WjtHff0Jt8aliTAoswNFY54fCWq3akaJ7uu0cFk1C2e-Mkl4BYP9vI2FpyqGoHG-ctV_IcVg=w600"
     },
     {
      "Serial No": 42,
      "Name": "CloneX #1296",
      "Collection": "CLONE X - X TAKASHI MURAKAMI",
-     "Last Price": "WETH 0.5512",
+     "Last Price": "WETH 0.55",
+     "Price in Number": 0.55,
+     "Price in USD": 1516.6360000000002,
      "URL": "https:\/\/lh3.googleusercontent.com\/xHJmuOgJ6dO1wYURbqnriQ7wQBvtRh-WZdm6Box64cLtJzR7VpZP0YwtKpzEzbvoUvYWHqFaQqlBj1VPphWocHTSNBPRix3G2eH0Gg=w600"
     },
     {
      "Serial No": 43,
      "Name": "CloneX #10860",
      "Collection": "CLONE X - X TAKASHI MURAKAMI",
-     "Last Price": "WETH 0.6174",
+     "Last Price": "WETH 0.61",
+     "Price in Number": 0.61,
+     "Price in USD": 1682.0872,
      "URL": "https:\/\/lh3.googleusercontent.com\/wtmbWMLCJ7QxsY41fv4O7hzQSr7g5ya_i3zAYXn_BH3-SzcKzhywf8t3aPXs4IIGpcQb_Gf4vt_AkCdcdlbXHRT3DZdlzqLO2IL1_9o=w600"
     },
     {
@@ -309,20 +395,26 @@ const nft =[
      "Name": "CloneX #3433",
      "Collection": "CLONE X - X TAKASHI MURAKAMI",
      "Last Price": "WETH 1.05",
+     "Price in Number": 1.05,
+     "Price in USD": 2895.396,
      "URL": "https:\/\/lh3.googleusercontent.com\/A-ToQqnmtAqKtZwvx68e6m2cV4HybPUcuvxxO1qEH5tbrz2CjkYq0oXOev0ZT4mOoTAZm78utfuIlAoNAyCc5EVpF60JBYnZ-hKX=w600"
     },
     {
      "Serial No": 45,
      "Name": "CloneX #8717",
      "Collection": "CLONE X - X TAKASHI MURAKAMI",
-     "Last Price": "WETH 11.7452",
+     "Last Price": "WETH 11.74",
+     "Price in Number": 11.74,
+     "Price in USD": 32373.2848,
      "URL": "https:\/\/lh3.googleusercontent.com\/XOEe6xz9E26GU4nnHWXYesbxHpRh2fLjpSSrZbfuXRyBVl8DLzZhgdFkXMeQhqbzXHFdIsOfGM-OcUrPjgo3GbrlUkqZp1vKwnjoMg=w600"
     },
     {
      "Serial No": 46,
      "Name": "CloneX #10387",
      "Collection": "CLONE X - X TAKASHI MURAKAMI",
-     "Last Price": "WETH 13.9345",
+     "Last Price": "WETH 13.93",
+     "Price in Number": 13.93,
+     "Price in USD": 38412.2536,
      "URL": "https:\/\/lh3.googleusercontent.com\/IOtnbQNztsvrcqCyJmDGCJTlzYXJR69pDHbgW1ylhQFjS99en_RqW-4U3BkyPf_tpnKs2i3v15P-VuYjlFmXRSVv-C_rB6LEbm2j5QY=w600"
     },
     {
@@ -330,6 +422,8 @@ const nft =[
      "Name": "CloneX #15196",
      "Collection": "CLONE X - X TAKASHI MURAKAMI",
      "Last Price": "WETH 14",
+     "Price in Number": 14,
+     "Price in USD": 38605.28,
      "URL": "https:\/\/lh3.googleusercontent.com\/M9_TBFqofkIAKsdrMv5UON40R36Y26YPMwzleU4cul1tRcJaew29e8IgAkdhA_Svpl3Vjf_5hXOKFtkHSPtbaEp7sqXhKWyg19M4_g=w600"
     },
     {
@@ -337,6 +431,8 @@ const nft =[
      "Name": "CloneX #5170",
      "Collection": "CLONE X - X TAKASHI MURAKAMI",
      "Last Price": "WETH 14.99",
+     "Price in Number": 14.99,
+     "Price in USD": 41335.2248,
      "URL": "https:\/\/lh3.googleusercontent.com\/Pi7rhlROvxnrn-WHrbJb9NsS1V-SFKTympVp031tUyhyGK1BDkDY5T5RMiHLsAaPBzSB10xPR4HS2a5hLmUIySMhL81h400LVAXWBQ=w600"
     },
     {
@@ -344,6 +440,8 @@ const nft =[
      "Name": "CloneX #12073",
      "Collection": "CLONE X - X TAKASHI MURAKAMI",
      "Last Price": "WETH 15",
+     "Price in Number": 15,
+     "Price in USD": 41362.8,
      "URL": "https:\/\/lh3.googleusercontent.com\/Se2_F3A-w9UPtxaP2hQvgJYRvW9v-8WNpOv9OR-sO9Oug97E321W6j9SZAAdXRU4_5rz9PYSxAQriIv7Uyi6wKsqqI37qDsfKth3Y9g=w600"
     },
     {
@@ -351,6 +449,8 @@ const nft =[
      "Name": "CloneX #18759",
      "Collection": "CLONE X - X TAKASHI MURAKAMI",
      "Last Price": "WETH 16",
+     "Price in Number": 16,
+     "Price in USD": 44120.32,
      "URL": "https:\/\/lh3.googleusercontent.com\/_FV-xHBJOMPw_90aYLoUGU5tSFM3fEwcyn19Ym9PUYwZX3thVzGwheKyXcMWuoo1Jc4PrtVZfKQf3a7CAQlwDVa3TRqnIcXaDuuHfQ=w600"
     }
    ];
